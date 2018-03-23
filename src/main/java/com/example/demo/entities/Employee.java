1418e07a4c39d0ca702a09c4544d6002c234a139
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-23 11:24:35 +0200

    Verification annotations added

--- src/main/java/com/example/demo/entities/Employee.java
+++ src/main/java/com/example/demo/entities/Employee.java
@@ -1,29 +1,33 @@
 package com.example.demo.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "employee")
 public class Employee {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @Size(min = 1, max = 50)
     @Column(name = "name")
     private String name;
 
+    @Size(min = 3, max = 20)
     @Column(name = "salary")
     private String salary;
 
+    @Email
+    @Size(min = 1, max = 50)
     @Column(name = "email")
     private String email;
 
     @Column(name = "departamentId")
-    private String departamentId;
+    private Departament departamentId;
 
 
 }
