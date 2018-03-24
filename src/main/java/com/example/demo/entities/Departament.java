453e5c77a5240824035deb79968b989329a9efb6
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-24 21:01:37 +0200

    Departamet entity modified

--- src/main/java/com/example/demo/entities/Departament.java
+++ src/main/java/com/example/demo/entities/Departament.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
-@Table(name = "departament")
 public class Departament {
 
     @Id
@@ -21,4 +20,45 @@ public class Departament {
 
     @OneToMany(mappedBy = "departamentId", cascade = CascadeType.ALL)
     private List<Employee> employee;
+
+    public Departament() {
+    }
+
+    public Departament(@NotEmpty @Size(min = 3, max = 150) String departament, List<Employee> employee) {
+        this.departament = departament;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(String departament) {
+        this.departament = departament;
+    }
+
+    public List<Employee> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(List<Employee> employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Departament{" +
+                "id=" + id +
+                ", departament='" + departament + '\'' +
+                ", employee=" + employee +
+                '}';
+    }
 }
