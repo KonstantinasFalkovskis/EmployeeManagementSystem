16b630a46abc5e7953e1c4740490afbec5a296b9
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-24 21:01:00 +0200

    Employee entity modified

--- src/main/java/com/example/demo/entities/Employee.java
+++ src/main/java/com/example/demo/entities/Employee.java
@@ -4,10 +4,8 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Entity
-@Table(name = "employee")
 public class Employee {
 
     @Id
@@ -32,9 +30,71 @@ public class Employee {
     private String email;
 
     @NotEmpty
-    @JoinColumn(name = "departamentId", referencedColumnName = "id")
-    @ManyToOne
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "departament_id", referencedColumnName = "id")
     private Departament departamentId;
 
+    public Employee() {
+    }
 
+    public Employee(@NotEmpty @Size(min = 1, max = 50) String name,
+                    @NotEmpty @Size(min = 3, max = 20) String salary,
+                    @NotEmpty @Email @Size(min = 1, max = 50) String email,
+                    @NotEmpty Departament departamentId) {
+        this.name = name;
+        this.salary = salary;
+        this.email = email;
+        this.departamentId = departamentId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Departament getDepartamentId() {
+        return departamentId;
+    }
+
+    public void setDepartamentId(Departament departamentId) {
+        this.departamentId = departamentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary='" + salary + '\'' +
+                ", email='" + email + '\'' +
+                ", departamentId=" + departamentId +
+                '}';
+    }
 }
