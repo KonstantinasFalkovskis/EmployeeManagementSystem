9fe6c6846c189af1b5f6c4d1c5b70dd7cc1a8fe2
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-26 10:25:08 +0300

    Employee repository modifiedL findByName method added

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -4,7 +4,12 @@ import com.example.demo.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByName(String name, Pageable pageable);
 
 }
