e96fbaa906f97e45f51977d62623efe5ecfed26b
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-26 10:41:23 +0300

    Employee service  was modified

--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -1,8 +1,18 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
 
+    //find exist emloyees by name with pagination
+    Page<Employee> findByName(String name, Pageable pageable);
+
+    //find exist employees list with pagination
+    Page<Employee> findAll(Pageable pageable);
+
+    
 }
