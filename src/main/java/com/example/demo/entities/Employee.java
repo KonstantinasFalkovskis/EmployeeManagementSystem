9973c3288a1802c57ec2711f05ff5a23197da3ed
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 17:00:32 +0300

    Some commites added

--- src/main/java/com/example/demo/entities/Employee.java
+++ src/main/java/com/example/demo/entities/Employee.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.03.28
+ * @version - v.1.0
+ */
 package com.example.demo.entities;
 
 import javax.persistence.*;
@@ -5,38 +10,54 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+/**
+ * Employee entity
+ */
 @Entity
 public class Employee {
 
+    //autoincrement value id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    //validations
+    //value name
     @NotEmpty
     @Size(min = 1, max = 50)
     @Column(name = "name")
     private String name;
 
+    //validations
+    //value salary
     @NotEmpty
     @Size(min = 3, max = 20)
     @Column(name = "salary")
     private String salary;
 
+    //validations
+    //value email
     @NotEmpty
     @Email
     @Size(min = 1, max = 50)
     @Column(name = "email")
     private String email;
 
+    //validations
+    //value departmentId
+    //relations between columns: reference ManyToOne
+    //columns departament_id & id from different tables joining
     @NotEmpty
     @ManyToOne(optional = false)
     @JoinColumn(name = "departament_id", referencedColumnName = "id")
     private Departament departamentId;
 
+    //default constructor
     public Employee() {
     }
 
+    //employee constructor
     public Employee(@NotEmpty @Size(min = 1, max = 50) String name,
                     @NotEmpty @Size(min = 3, max = 20) String salary,
                     @NotEmpty @Email @Size(min = 1, max = 50) String email,
@@ -46,7 +67,7 @@ public class Employee {
         this.email = email;
         this.departamentId = departamentId;
     }
-
+    //getters & setters for values
     public Long getId() {
         return id;
     }
