c3f93cf0635e1bb5ac162dc570c658e9fc68823e
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 16:10:11 +0300

    EmployeeServiceImpl was modified: methods from EmployeeService implements

--- src/main/java/com/example/demo/services/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/services/EmployeeServiceImpl.java
@@ -1,5 +1,39 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 public class EmployeeServiceImpl implements EmployeeService {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Page<Employee> findByName(String name, Pageable pageable) {
+        return employeeRepository.findByName(name, pageable);
+    }
+
+    @Override
+    public Page<Employee> findAll(Pageable pageable) {
+        return employeeRepository.findAll(pageable);
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
