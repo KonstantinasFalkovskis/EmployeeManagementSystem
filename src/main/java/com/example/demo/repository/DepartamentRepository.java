7403129dfd21d6b49abd9c49f1e574d82dded6cd
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 16:19:07 +0300

    Department repo was modified

--- src/main/java/com/example/demo/repository/DepartamentRepository.java
+++ src/main/java/com/example/demo/repository/DepartamentRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.entities.Departament;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartamentRepository extends JpaRepository<Departament, Long>{
+
+    //get department by searching name
+    public Page<Departament> findDepoByName(String name, Pageable pageable);
 
 }
