298ef07eebf17dd19a5bc01aefa378a9b2e5dbb2
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 16:08:08 +0300

    Employee repository was modified

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,15 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entities.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.awt.print.Pageable;
-import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    List<Employee> findByName(String name, Pageable pageable);
+    Page<Employee> findByName(String name, Pageable pageable);
 
 }
