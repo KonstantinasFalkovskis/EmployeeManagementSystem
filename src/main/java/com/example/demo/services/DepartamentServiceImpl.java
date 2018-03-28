d509622d906e09a448316694a76f6fcaeeec6daa
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 16:34:47 +0300

    Some @JavaDocs added into

--- src/main/java/com/example/demo/services/DepartamentServiceImpl.java
+++ src/main/java/com/example/demo/services/DepartamentServiceImpl.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.03.28
+ * @version - v.1.0
+ */
 package com.example.demo.services;
 
 import com.example.demo.entities.Departament;
@@ -6,31 +11,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/**
+ * Methods implementations for Department entity
+ */
 public class DepartamentServiceImpl implements DepartamentService {
 
+    //Department repository reaching
     @Autowired
     private DepartamentRepository departamentRepository;
 
+    //department from data base by name. Pagination included also;
     @Override
     public Page<Departament> findDepoByName(String name, Pageable pageable) {
         return departamentRepository.findDepoByName(name, pageable);
     }
 
+    //departments list from data base + pagination;
     @Override
     public Page<Departament> findAll(Pageable pageable) {
         return departamentRepository.findAll(pageable);
     }
 
+    //department calling from list by id
     @Override
     public Departament findOne(Long id) {
         return departamentRepository.findById(id).orElse(null);
     }
 
+    //department adding to data base
     @Override
     public Departament saveDepo(Departament departament) {
         return departamentRepository.save(departament);
     }
 
+    //department removing from data base by id value
     @Override
     public void deleteDepo(Long id) {
         departamentRepository.deleteById(id);
