99c53e1e59ce2374f117deeed39938d444b020e7
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 16:20:25 +0300

    Some methods was added into DepartmentService

--- src/main/java/com/example/demo/services/DepartamentService.java
+++ src/main/java/com/example/demo/services/DepartamentService.java
@@ -1,7 +1,27 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Departament;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface DepartamentService {
+
+    //get department by name
+    public Page<Departament> findDepoByName(String name, Pageable pageable);
+
+    //get exist deprtaments list
+    public Page<Departament> findAll(Pageable pageable);
+
+    //get department info by id
+    public Departament findOne(Long id);
+
+    //save department into DB
+    public Departament saveDepo(Departament departament);
+
+    //remove department from data base
+    public void deleteDepo(Long id);
+
+
 }
