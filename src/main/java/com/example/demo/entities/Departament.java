676c679e9f85acbf76717dbe4265be2d18764a54
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 17:00:05 +0300

    Some commites added

--- src/main/java/com/example/demo/entities/Departament.java
+++ src/main/java/com/example/demo/entities/Departament.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.03.28
+ * @version - v.1.0
+ */
 package com.example.demo.entities;
 
 import javax.persistence.*;
@@ -5,30 +10,41 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.List;
 
+/**
+ * Department entity
+ */
 @Entity
 public class Departament {
 
+    //autoincrement value id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    //validations
+    //value departament
     @NotEmpty
     @Size(min = 3, max = 150)
     @Column(name = "departament")
     private String departament;
 
+    //value employee -> type List<>
+    //relations between columns: reference OneToMany -> in this case many employees works only in one department
+    //reference to departamentId value from Employees class, cascade type = All
     @OneToMany(mappedBy = "departamentId", cascade = CascadeType.ALL)
     private List<Employee> employee;
 
+    //default constructor
     public Departament() {
     }
 
+    //department constructor
     public Departament(@NotEmpty @Size(min = 3, max = 150) String departament, List<Employee> employee) {
         this.departament = departament;
         this.employee = employee;
     }
-
+    //getters and setters
     public Long getId() {
         return id;
     }
