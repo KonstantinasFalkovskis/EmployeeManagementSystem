d7b4f6a788b3259cff5d05d2a03bfc7599f2f5ed
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-28 17:26:32 +0300

    Controller for Index page created

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,12 +1,20 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.03.28
+ * @version - v.1.0
+ */
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+//controller for index.html calling
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @RequestMapping(name = "/", method = RequestMethod.GET)
     public String showPage() {
         return "index";
     }
