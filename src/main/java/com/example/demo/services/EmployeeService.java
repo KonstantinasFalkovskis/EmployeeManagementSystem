be1e855e53801ec03ae98536369ce28da342bc08
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-29 15:40:54 +0300

    modified

--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -5,6 +5,7 @@
  */
 package com.example.demo.services;
 
+import com.example.demo.entities.Departament;
 import com.example.demo.entities.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,22 +14,21 @@ import org.springframework.stereotype.Service;
 /**
  * Employees services
  */
-@Service
 public interface EmployeeService {
 
     //find exist emloyees by name with pagination
-    public Page<Employee> findByName(String name, Pageable pageable);
+    Page<Employee> findByName(String name, Pageable pageable);
 
     //get exist employees list with pagination
-    public Page<Employee> findAll(Pageable pageable);
+    Page<Employee> findAll(Pageable pageable);
 
     //get employee by id
-    public Employee getEmployeeById(Long id);
+    Employee getEmployeeById(Long id);
 
     //delete employee by id
-    public void deleteEmployee(Long id);
+    void deleteEmployee(Long id);
 
     //save employee
-    public Employee saveEmployee(Employee employee);
+    Employee saveEmployee(Employee employee);
 
 }
