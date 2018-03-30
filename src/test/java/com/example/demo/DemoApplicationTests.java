e5831b9eddb0d2f26cedd162a15a4175cfa2e0dc
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-30 19:36:09 +0300

    @JavaDocs +

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.03.30
+ * @version - v.1.0
+ */
 package com.example.demo;
 
 import com.example.demo.entities.Departament;
@@ -8,7 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -17,16 +21,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+/**
+ * Tests for departments + employees
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+    //calling depo services
 	@Autowired
 	private DepartamentService departamentService;
 
+	//calling empl services
 	@Autowired
     private EmployeeService employeeService;
 
+    /**
+     * Test for new department adding
+     */
 	@Test
 	public void addDepartment() {
 		Departament departament = new Departament(10L, "Accounting");
@@ -35,6 +47,9 @@ public class DemoApplicationTests {
 		assertEquals(departament.getDepartament(), "Accounting");
 	}
 
+    /**
+     * Test for exist department editing
+     */
 	@Test
 	public void editDepo() {
 		Departament depo = departamentService.findOne(5L);
@@ -45,6 +60,9 @@ public class DemoApplicationTests {
 		assertEquals(updatedDepo.getDepartament(), "Management");
 	}
 
+    /**
+     * Test for existing department viewing
+     */
 	@Test
 	public void viewDepo() {
 		Departament depo = departamentService.findOne(5L);
@@ -52,15 +70,23 @@ public class DemoApplicationTests {
 		assertEquals(depo.getDepartament(), "Management");
 	}
 
+    /**
+     * Test for exist department and if exist all employees inside removing
+     */
 	@Test
 	public void deleteDepo() {
-		Departament departament = departamentService.findOne(9L);
+		Departament departament = departamentService.findOne(12L);
 		if (departament.getId() != null) {
-			departamentService.deleteDepo(9L);
+			departamentService.deleteDepo(12L);
 			employeeService.deleteEmployee(departament.getId());
+            assertNull(departament);
+            assertEquals(departament.getDepartament(), null);
 		}
 	}
 
+    /**
+     * Test for new employee adding
+     */
 	@Test
     public void addNewEmployee() {
         Employee employee = new Employee("John McLain", "3500", "john@yahoo.com", null);
@@ -73,6 +99,9 @@ public class DemoApplicationTests {
         assertEquals(employee2.getName(), "Alex Fox");
     }
 
+    /**
+     * Test for exist employee view
+     */
     @Test
     public void showEmployee() {
         Employee employee = employeeService.getEmployeeById(3L);
@@ -80,6 +109,9 @@ public class DemoApplicationTests {
         assertEquals(employee.getName(), "Alex Fox");
     }
 
+    /**
+     * Test for exist employee updating
+     */
     @Test
     public void editEmploye() {
         Employee employee = employeeService.getEmployeeById(3L);
@@ -91,6 +123,9 @@ public class DemoApplicationTests {
         assertEquals(employee.getName(), "Test Test");
     }
 
+    /**
+     * Test for exist employee removing
+     */
     @Test
     public void delEmployeeByName() {
 	    Employee employee = employeeService.getEmployeeById(3L);
