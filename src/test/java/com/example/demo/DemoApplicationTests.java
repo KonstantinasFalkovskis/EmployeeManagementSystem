7d52eda6e72b0bb5bbb9720f5ecf6df9c2cf6c0f
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-30 14:34:14 +0300

    Test for department added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example.demo;
 
+import com.example.demo.entities.Departament;
+import com.example.demo.services.DepartamentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private DepartamentService departamentService;
+
 	@Test
-	public void contextLoads() {
+	public void addDepartment() {
+		Departament departament = new Departament(1L, "Accounting");
+		assertNotNull(departament);
+		departamentService.saveDepo(departament);
+		assertEquals(departament.getDepartament(), "Accounting");
+	}
+
+	@Test
+	public void editDepo() {
+		Departament depo = departamentService.findOne(1L);
+		assertNotNull(depo);
+		Departament updatedDepo = new Departament(1L, "Management");
 	}
 
 }
