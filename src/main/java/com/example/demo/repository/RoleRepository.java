c78da0ad3ae31d6e18fee669e2e87a1ffe1e8281
Konstantinas Falkovskis <integro@inbox.lt>
2018-04-03 11:48:08 +0300

    @JavaDocs + commites added

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.04.02
+ * @version - v.1.0
+ */
 package com.example.demo.repository;
 
 import com.example.demo.entities.Role;
@@ -6,8 +11,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Roles repository
+ */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
 
+    /**
+     * Searching roles by role
+     * @param role
+     * @return
+     */
     List<Role> findByRole(Role role);
 }
