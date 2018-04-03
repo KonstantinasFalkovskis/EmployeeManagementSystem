a517dc863000eb06a085be0534e42df6349f18b9
Konstantinas Falkovskis <integro@inbox.lt>
2018-04-03 11:47:01 +0300

    @JavaDocs + commites added

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.04.02
+ * @version - v.1.0
+ */
 package com.example.demo.repository;
 
 import com.example.demo.entities.Task;
@@ -7,9 +12,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Task repository
+ */
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+ /**
+  * Searching tasks by user
+  * @param user
+  * @return
+  */
  List<Task> findByUser(User user);
 
 }
