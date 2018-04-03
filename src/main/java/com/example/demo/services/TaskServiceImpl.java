031cb501829c20f5ba9d23d35203255fa142d4d9
Konstantinas Falkovskis <integro@inbox.lt>
2018-04-03 11:16:51 +0300

    task service was modified

--- src/main/java/com/example/demo/services/TaskServiceImpl.java
+++ src/main/java/com/example/demo/services/TaskServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.demo.entities.Task;
 import com.example.demo.entities.User;
 import com.example.demo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
     @Autowired
