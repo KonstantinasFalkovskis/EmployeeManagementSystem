fe97159e494da9bf107bb82ff96cf53320b34b43
Konstantinas Falkovskis <integro@inbox.lt>
2018-04-03 11:37:46 +0300

    @JavaDocs + commites added

--- src/main/java/com/example/demo/controllers/TaskController.java
+++ src/main/java/com/example/demo/controllers/TaskController.java
@@ -1,3 +1,8 @@
+/**
+ * @Author - Falco Constantine
+ * @date - 2018.04.03
+ * @version - v.1.0
+ */
 package com.example.demo.controllers;
 
 import com.example.demo.entities.Task;
@@ -13,15 +18,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+/**
+ * New task controller
+ */
 @Controller
 public class TaskController {
 
+    //Task services calling
     @Autowired
     private TaskServiceImpl taskServiceImpl;
 
+    //Users services calling
     @Autowired
     private UserService userService;
 
+    /**
+     *
+     * @param email
+     * @param model
+     * @param session
+     * @return
+     */
     @GetMapping("/addTask")
     public String taskForm(String email, Model model, HttpSession session) {
         session.setAttribute("email", email);
@@ -29,6 +46,13 @@ public class TaskController {
         return "views/taskForm";
     }
 
+    /**
+     * New task adding
+     * @param task
+     * @param bindingResult
+     * @param session
+     * @return
+     */
     @PostMapping("/addTask")
     public String addTask(@Valid Task task, BindingResult bindingResult, HttpSession session) {
         if(bindingResult.hasErrors()) {
