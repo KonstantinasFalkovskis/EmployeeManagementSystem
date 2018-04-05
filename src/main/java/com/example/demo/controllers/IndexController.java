8ea845289cd4282a9f671860b12e9dabe49bbc1b
Konstantinas Falkovskis <integro@inbox.lt>
2018-04-05 19:33:53 +0300

    modified

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -7,14 +7,12 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 //controller for index.html calling
 @Controller
 public class IndexController {
 
-    @GetMapping(name = "/")
+    @GetMapping("/")
     public String showPage() {
         return "index";
     }
